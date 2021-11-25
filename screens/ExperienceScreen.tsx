a33d4090d6ffb1a2d0e769eec01b9f1113e51198
Justice Velasco <justicevgomez001@gmail.com>
2021-11-25 23:25:13 +0100

    open url implemented

--- screens/ExperienceScreen.tsx
+++ screens/ExperienceScreen.tsx
@@ -1,6 +1,8 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Video } from "expo-av";
+import { openURL } from "expo-linking";
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
 
@@ -22,24 +24,46 @@ const ExperienceScreen = () => {
         <Video
           ref={video}
           style={styles.media}
-          source={
-            require("../assets/videos/timelapse.mp4")
-          }
+          source={require("../assets/videos/timelapse.mp4")}
           resizeMode="contain"
           isLooping
           shouldPlay
         />
-        <View>
-          <Text style={styles.text}>
-            Esta es para mi, una oportunidad única de poner en práctica todo lo
-            aprendido durante los meses de estudio y formación. {"\n"} La
-            juventud es un grado en cuanto a rapidez de aprendizaje y
-            asimilación de tareas. Esta es para mi, una oportunidad única de
-            poner en práctica todo lo aprendido durante los meses de estudio y
-            formación. La juventud es un grado en cuanto a rapidez de
-            aprendizaje y asimilación de tareas.
-          </Text>
+        <Text style={styles.text}>
+          Durante un mes programé una pequeña aplicación web del tipo Blog en
+          una agencia de marketing y software a medida ubicada en Reus,
+          Tarragona, dicha aplicación web fue creada en Laravel 8, contó con una
+          pequeña área administrativa donde se podía crear, actualizar o
+          eliminar las publicaciones. Área la cual solo los usuarios
+          seleccionados en el area administrativa podían tener acceso a ella.
+        </Text>
+      </View>
+
+      <View style={styles.cardContainer}>
+        <View style={{ flexDirection: "row" }}>
+          <Image
+            source={require("../assets/images/foto.jpg")}
+            style={styles.cardAvatar}
+          />
+          <View>
+            <Text style={styles.nameHeader}>Jusav</Text>
+            <Text style={styles.aboutScreen}>Projectos Realizados</Text>
+          </View>
         </View>
+        <Video
+          ref={video}
+          style={styles.media}
+          source={require("../assets/videos/timelapse.mp4")}
+          resizeMode="contain"
+          isLooping
+          shouldPlay
+        />
+        <Text style={styles.text}>
+          Hello
+        </Text>
+        <TouchableOpacity onPress={() => openURL("https://github.com/Jusav503?tab=repositories")}>
+          <FontAwesome name="github" size={24} color="white" />
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
