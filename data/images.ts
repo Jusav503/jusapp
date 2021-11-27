d49553f25e230b67cb0533e5fd2b661ea8383e25
Justice Velasco <justicevgomez001@gmail.com>
2021-11-27 23:53:29 +0100

    data file images for carousel created

---
+++ data/images.ts
@@ -0,0 +1,10 @@
+export default {
+    experienceImages:[
+        'https://jusapp.s3.eu-central-1.amazonaws.com/teslaimage.jpg',
+        'https://jusapp.s3.eu-central-1.amazonaws.com/amazonimage.jpg',
+        'https://jusapp.s3.eu-central-1.amazonaws.com/amazonimage2.jpg',
+        'https://jusapp.s3.eu-central-1.amazonaws.com/tiktokimage.jpg',
+        'https://jusapp.s3.eu-central-1.amazonaws.com/tiktokimage2.jpg',
+        'https://jusapp.s3.eu-central-1.amazonaws.com/netflix.jpg',
+    ]
+}
--- screens/ExperienceScreen.tsx
+++ screens/ExperienceScreen.tsx
@@ -1,10 +1,11 @@
-import { FontAwesome } from "@expo/vector-icons";
 import { Video } from "expo-av";
 import { openURL } from "expo-linking";
 import React from "react";
 import { Image, ScrollView, Text, View, TouchableOpacity } from "react-native";
-import MyButton from "../components/MyButton";
 
+import ImageCarousel from "../components/ImageCarousel";
+import MyButton from "../components/MyButton";
+import images from "../data/images";
 import styles from "./styles";
 
 const ExperienceScreen = () => {
@@ -51,12 +52,19 @@ const ExperienceScreen = () => {
             <Text style={styles.aboutScreen}>Projectos Realizados</Text>
           </View>
         </View>
-        
+        <ImageCarousel images={images.experienceImages} />
         <Text style={styles.text}>
-          He realizado "features" como algunas aplicaciones en el mundo del internet, por ejemplo: likes dinamicos como los de Twitter, el poder mostrar listas de reproducción (almacenadas en AWS amplify) reproducir y pausar una canción como en Spotify... También he realizado una amplia replica de UI y algunas "features" de algunas aplicaciones tales como: Netflix, la web de Tesla, WhatsApp, Amazon y TikTok.
+          He realizado "features" como las de algunas aplicaciones móviles, por ejemplo: likes dinamicos como los de Twitter o TikTok, el poder
+          mostrar listas de reproducción (almacenadas en AWS amplify) reproducir o pausar una canción, mostrar la barra de progreso de la canción como en Spotify... También he realizado una amplia replica de UI's de algunas aplicaciones tales como: Netflix, Tesla, WhatsApp, Amazon y TikTok.
         </Text>
-        <TouchableOpacity onPress={() => openURL("https://github.com/Jusav503?tab=repositories")}>
-          <MyButton FontAwesomeIcon="github" colorIcon="white" title="Jusav503" colorTitle="white" backgroundColor="#171B22" />
+        <TouchableOpacity onPress={() => openURL("https://github.com/Jusav503?tab=repositories")} >
+          <MyButton
+            FontAwesomeIcon="github"
+            colorIcon="white"
+            title="Jusav503"
+            colorTitle="white"
+            backgroundColor="#171B22"
+          />
         </TouchableOpacity>
       </View>
     </ScrollView>
