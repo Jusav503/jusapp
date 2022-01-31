0b0665d0ecfeb8a4d47c8a900174452efcf437cd
Justice Velasco <justicevgomez001@gmail.com>
2022-01-31 04:05:49 +0100

    🔨 button uptaded

--- screens/SkillsScreen.tsx
+++ screens/SkillsScreen.tsx
@@ -1,6 +1,5 @@
-import { openURL } from "expo-linking";
 import React from "react";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 
 import styles from "./styles";
 import MyButton from "../components/MyButton";
@@ -9,8 +8,11 @@ const SkillsScreen = () => {
   return (
     <ScrollView style={styles.container}>
       <View style={styles.cardContainer}>
-        <View style={{flexDirection:"row"}}>
-          <Image source={require("../assets/images/foto.jpg")} style={styles.cardAvatar} />
+        <View style={{ flexDirection: "row" }}>
+          <Image
+            source={require("../assets/images/foto.jpg")}
+            style={styles.cardAvatar}
+          />
           <View>
             <Text style={styles.nameHeader}>Jusav</Text>
             <Text style={styles.aboutScreen}>Skills</Text>
@@ -22,25 +24,43 @@ const SkillsScreen = () => {
         />
         <View>
           <Text style={styles.text}>
-            Una de las habilidades que me destacan es el ser una persona proactiva, demostrado en la formación que realicé para técnico programación e implementación de E-Commerce, webs y aplicaciones de venta. A pesar que llevo una corta trayectoria en el mundo de la programación me veo muy motivado y con ganas de seguir aprendiendo, ya sea por medio de formaciones o por mi propia cuenta.
+            Una de las habilidades que me destacan es el ser una persona
+            proactiva, demostrado en la formación que realicé para técnico
+            programación e implementación de E-Commerce, webs y aplicaciones de
+            venta. A pesar que llevo una corta trayectoria en el mundo de la
+            programación me veo muy motivado y con ganas de seguir aprendiendo,
+            ya sea por medio de formaciones o por mi propia cuenta.
           </Text>
         </View>
       </View>
+
       <View style={styles.cardContainer}>
-        <View style={{flexDirection:"row"}}>
-          <Image source={{uri: "https://media-exp1.licdn.com/dms/image/C4D03AQGOJEDGFZpuCg/profile-displayphoto-shrink_800_800/0/1516867329561?e=1643846400&v=beta&t=S83-svgl-R0PzBFL6LCMqHvb9QPDTEsmHe89j0pp_pA"}} style={styles.cardAvatar} />
+        <View style={{ flexDirection: "row" }}>
+          <Image
+            source={{
+              uri: "https://media-exp1.licdn.com/dms/image/C4D03AQGOJEDGFZpuCg/profile-displayphoto-shrink_800_800/0/1516867329561?e=1643846400&v=beta&t=S83-svgl-R0PzBFL6LCMqHvb9QPDTEsmHe89j0pp_pA",
+            }}
+            style={styles.cardAvatar}
+          />
           <View>
             <Text style={styles.nameHeader}>Edgar Costilla</Text>
             <Text style={styles.aboutScreen}>Formador</Text>
           </View>
         </View>
+
         <View>
           <Text style={styles.text}>
-            "Justice es una persona que le encanta este mundillo, a pesar de la edad que tiene sus ganas por aprender son increíbles"
+            "Justice es una persona que le encanta este mundillo, a pesar de la
+            edad que tiene sus ganas por aprender son increíbles"
           </Text>
-          <TouchableOpacity onPress={() => openURL("https://www.linkedin.com/in/edgarcostilla/")}>
-            <MyButton title="Edgar Costilla" colorTitle="white" FontAwesomeIcon="linkedin-square" colorIcon="white" backgroundColor="#0A66C2" />
-          </TouchableOpacity>
+          <MyButton
+            title="Edgar Costilla"
+            colorTitle="white"
+            FontAwesomeIcon="linkedin-square"
+            colorIcon="white"
+            backgroundColor={["#4C9ED6", "#0A66C2"]}
+            link="https://www.linkedin.com/in/edgarcostilla/"
+          />
         </View>
       </View>
     </ScrollView>
